c21d8f20a54b9a6cfbcc6f9d28d7306166728cca
5staruser <100305686+5staruser@users.noreply.github.com>
2022-06-10 19:00:03 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,32 @@
+const socket=io("http://localhost:8000");
+const form=document.getElementById("send-container");
+const messageInput=document.getElementsByClassName("messageinp")
+const messagecontainer=document.querySelector(".container");
+var audio=new Audio("ting.mp3")
+const append=(message,position)=>{
+    const messageelement=document.createElement("div")
+    messageelement.innerText=message
+    messageelement.classList.add("message")
+    messageelement.classList.add(position)
+    messagecontainer.append(messageelement)
+    if(position=="left"){
+        audio.play()
+    }
+}
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    var message=messageInput.value
+    append(`You : ${message}`,"right")
+    socket.emit("send",message)
+})
+const name=prompt("enter your name to join");
+socket.emit("newuser-joined",name);
+socket.on("user-joined",name=>{
+    append(`${name} joined the chat`,"right")
+})
+socket.on("recieve",data=>{
+    append(`${data.name}:${data.message}`,"left")
+})
+socket.on("left",name=>{
+    append(`${name} left the chat`,"left")
+})
